b669b5ca5d86d77ce6dc5066e9a33b4fa71c95d2
Hessvacio Hassan <hhassan@oswego.edu>
2023-02-21 20:12:57 -0500

    Finished L3 State Management

--- L3. State Management/3. Managing State/Starter/src/App.js
+++ L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,12 +1,18 @@
 import corgi from "./corgi.jpg";
+import { useState } from 'react';
 import "./App.css";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="container">
       <h2>Like this photo!</h2>
       <img src={corgi} alt="Corgi"></img>
-      <p>Amount of likes: </p>
+      <p>Amount of likes: {count}</p>
+            <button onClick={() => setCount(count + 1)}>
+        Like
+      </button>
     </div>
   );
 };
